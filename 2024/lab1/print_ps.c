58f62a03ce2f0f6b67cbb31f687cdb1948c3ca54
vq <1457480465@qq.com>
2024-03-01 00:41:16 +0800

    os-learing 2024 lab1

--- 2024/lab1/print_ps.c
+++ 2024/lab1/print_ps.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    FILE *fp;
-    char buffer[1024];
-
-    fp = popen("ps -e | iconv -f ISO-8859-1 -t UTF-8", "r");
-    if (fp == NULL) {
-        printf("Failed to run command");
-        exit(1);
-    }
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-        printf("%s", buffer);
-    }
-
-    pclose(fp);
+int main() {
+    // write code 
     return 0;
 }
