e53e6d10f929af9fa1f62c1e450ca2be939f43b7
vq <1457480465@qq.com>
2024-04-06 22:38:01 +0800

    09-并发控制 同步(生产者-消费者, 条件变量)

---
+++ 2024/lab9/pc-1.c
@@ -0,0 +1,36 @@
+#include "thread.h"
+#include <thread-sync.h>
+
+mutex_t lk = MUTEX_INIT();
+
+int n, depth = 0;
+
+void T_produce() {
+    while (1) {
+retry:
+        mutex_lock(&lk);
+        int ready = (depth < n);
+        mutex_unlock(&lk);
+        if (!ready) goto retry;
+
+        mutex_lock(&lk);
+        printf("(");
+        depth++;
+        mutex_unlock(&lk);
+    }
+}
+
+void T_consume() {
+    while (1) {
+retry: 
+        mutex_lock(&lk);
+        int ready = (depth > 0);
+        mutex_unlock(&lk);
+        if (!ready) goto retry;
+
+        mutex_lock(&lk);
+        printf(")");
+        depth--;
+        mutex_unlock(&lk);
+    }
+}
